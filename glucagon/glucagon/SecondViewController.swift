04316a432deb61072f8a7827dfa4cda6c9748cd1
sahasd <sahasd@gmail.com>
2017-02-05 06:53:11 -0800

    finished add

--- glucagon/glucagon/SecondViewController.swift
+++ glucagon/glucagon/SecondViewController.swift
@@ -8,21 +8,24 @@
 
 import UIKit
 import Alamofire
+import Firebase
 
-var mainJSON : JSON = JSON.null;
-
+var ref = FIRDatabase.database().reference()
 
 
 class SecondViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    @IBAction func addImage(_ sender: Any) {
-        
-        
-    }
-    @IBAction func removeImage(_ sender: Any) {
-    }
+    var mainJSON : JSON = JSON.null;
     
-    @IBOutlet weak var no: UIButton!
+    @IBAction func addImage(_ sender: Any) {
+        print("added image");
+    }
+
+    
     @IBOutlet weak var yes: UIButton!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var calories: UILabel!
+    @IBOutlet weak var sugar: UILabel!
+    @IBOutlet weak var protein: UILabel!
     
     @IBAction func takePic(_ sender: Any) {
         print("pressed button");
@@ -31,7 +34,11 @@ class SecondViewController: UIViewController, UINavigationControllerDelegate, UI
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        name.isHidden = true;
+        yes.isHidden = true;
+        calories.isHidden = true;
+        sugar.isHidden = true;
+        protein.isHidden = true;
     }
 
     override func didReceiveMemoryWarning() {
@@ -82,12 +89,35 @@ class SecondViewController: UIViewController, UINavigationControllerDelegate, UI
         let dataStr = imageData!.base64EncodedString(options: [.lineLength64Characters])
         print(dataStr)
         let parameters = ["pic":dataStr]
-        Alamofire.request("https://los-altos-hacks-2-nihaleg.c9users.io/pic", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
-            if let JSON = response.result.value {
-                mainJSON = JSON as! JSON;
-                print (JSON)
-            }
+        Alamofire.request("https://los-altos-hacks-2-nihaleg.c9users.io/pic", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseString { response in
+                var str = response.result.value!
+                if let dataFromString = str.data(using: .utf8, allowLossyConversion: false) {
+                    let json = JSON(data: dataFromString)
+                    print(json)
+                    self.name.text = "Name: \(json["item"].string!)"
+                    for(index, subJson) in json["nutrients"]
+                    {
+                        if(subJson["nutrient"] == "Protein" || subJson["nutrient"] == "protein")
+                        {
+                            self.protein.text = "Protein: \(subJson["amount"].string!) grams"
+                        }
+                        if(subJson["nutrient"] == "Calories" || subJson["nutrient"] == "calories")
+                        {
+                            self.calories.text = "Calories: \(subJson["amount"].string!) grams"
+                        }
+                        if(subJson["nutrient"] == "Sugars" || subJson["nutrient"] == "sugars")
+                        {
+                            self.sugar.text = "Sugars: \(subJson["amount"].string!) grams"
+                        }
+                    }
+                    
+                }
         }
+        name.isHidden = false;
+        yes.isHidden = false;
+        calories.isHidden = false;
+        sugar.isHidden = false;
+        protein.isHidden = false;
     }
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
